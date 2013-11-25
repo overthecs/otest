2846b3b9072165920261eb38237c62c865e51abc
dewsy <BeiYuu@WangmatoMacBook-Air-3.local>
2013-11-25 15:51:09 +0800

    style inline and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    compass: {
+      dev: {
+        options: {
+          cssDir: 'static',
+          sassDir: 'static',
+          environment: 'development'
+        }
+      },
+      pub: {
+        options: {
+          cssDir: 'static',
+          sassDir: 'static',
+          environment: 'production'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['static/*.scss'],
+        tasks: ['compass']
+      }
+    },
+    connect: {
+      site1: {
+        options: {
+          port: 9001
+          //Note that if this option is enabled, any tasks specified after this task will never run.
+          //see there https://github.com/gruntjs/grunt-contrib-connect#keepalive
+          //so use `grunt server:keepalive`
+          //keepalive: true
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', 'compass:pub');
+  grunt.registerTask('server', ['connect', 'watch']);
+}
